ba3ed8b5a732a893247596965bac7e21f4b9624b
e-ago <elena.ago@gmail.com>
2017-08-03 16:37:43 -0700

    run script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+./CUDA_version/bitcracker_cuda -h
 
 ./CUDA_version/bitcracker_cuda -i ./Images/imgWin8 -d ./Dictionary/dummy.txt -t 1 -k 1 -g 0
+
+./CUDA_version/bitcracker_cuda -i ./Images/imgWin7 -d ./Dictionary/dummy.txt -t 1 -k 1 -g 0
+
