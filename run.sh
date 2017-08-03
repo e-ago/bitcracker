705f94f37eae1832f00df7eb708b0ad6d7123b6d
e-ago <elena.ago@gmail.com>
2017-08-03 16:32:08 -0700

    run script fix

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-./CUDA_version/bitcracker_cuda -i ../Images/imgWin8 -d ../Dictionary/dummy.txt -t 1 -k 1 -g 0
+./CUDA_version/bitcracker_cuda -i ./Images/imgWin8 -d ./Dictionary/dummy.txt -t 1 -k 1 -g 0
