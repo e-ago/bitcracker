52ef56d5b2cf7ad696ac8f88eb08da039eb3406f
elenago <elenago@elenagos-MBP.lan>
2017-09-14 20:01:43 +0200

    print values VMK

--- OpenCL_version/utils.c
+++ OpenCL_version/utils.c
@@ -130,15 +130,15 @@ int readData(char * encryptedImagePath, unsigned char ** salt, unsigned char ** 
 				fillBuffer(encryptedImage, *encryptedVMK, VMK_SIZE);
 
 				fprintf(stdout, "Nonce:\n");
-				print_hex(nonce, NONCE_SIZE);
+				print_hex(*nonce, NONCE_SIZE);
 				fprintf(stdout, "\n");
 
 				fprintf(stdout, "MAC:\n");
-				print_hex(mac, MAC_SIZE);
+				print_hex(*mac, MAC_SIZE);
 				fprintf(stdout, "\n");
 
 				fprintf(stdout, "VMK:\n");
-				print_hex(encryptedVMK, VMK_SIZE);
+				print_hex(*encryptedVMK, VMK_SIZE);
 				fprintf(stdout, "\n");
 				break;
 			}
