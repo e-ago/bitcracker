5e20bd8400909b136b1b2dbe2ae42d040d20b625
Elena Agostini <eagostini@nvidia.com>
2021-11-19 17:33:00 +0100

    Reintroduce license removed by mistake

--- src_CUDA/cuda_attack.cu
+++ src_CUDA/cuda_attack.cu
@@ -1,3 +1,24 @@
+/*
+ * BitCracker: BitLocker password cracking tool, CUDA version.
+ * Copyright (C) 2013-2017  Elena Ago <elena dot ago at gmail dot com>
+ *                                                      Massimo Bernaschi <massimo dot bernaschi at gmail dot com>
+ *
+ * This file is part of the BitCracker project: https://github.com/e-ago/bitcracker
+ *
+ * BitCracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * BitCracker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BitCracker. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "bitcracker.h"
 
 int			*deviceFound, *hostFound;
